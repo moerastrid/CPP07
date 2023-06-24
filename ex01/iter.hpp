fd5b25a6bfe2884e71d546a032be11b389557cdf
moerastrid@gmail.com <moerastrid@gmail.com>
2023-06-24 14:24:55 +0200

    setup CPP07 ex01, finish simple iter

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -6,11 +6,18 @@
 /*   By: ageels <ageels@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/06/07 16:49:27 by ageels        #+#    #+#                 */
-/*   Updated: 2023/06/07 16:49:45 by ageels        ########   odam.nl         */
+/*   Updated: 2023/06/24 14:23:00 by astrid        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_HPP
 #define ITER_HPP
+
+template<typename T, typename U, typename V>
+void my_iter(T &my_array, U len, void (*func)(V)) {
+    for (int i(0); i < len; i++) {
+        func(my_array[i]);
+    }
+};
 
 #endif
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,13 +6,42 @@
 /*   By: ageels <ageels@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/06/07 16:48:07 by ageels        #+#    #+#                 */
-/*   Updated: 2023/06/07 16:50:49 by ageels        ########   odam.nl         */
+/*   Updated: 2023/06/24 14:24:13 by astrid        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "test.hpp"
+#include <string>
+#include <iostream>
 #include "iter.hpp"
 
+static void toupper(std::string &s) {
+	int len = s.length();
+	for (int i(0); i < len; i++) {
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+	}
+}
+
+void tolower(std::string &s) {
+	int len = s.length();
+	for (int i(0); i < len; i++) {
+		if (s[i] >= 'A' && s[i] <= 'Z')
+			s[i] += 32;
+	}
+}
+
 int main(void) {
-	test my_array[] = {test("one"), test("two"), test("three")};
+	int len = 3;
+	
+	// int count[] = {1, 2, 3};
+	std::string names[] = {"Astrid", "Riek", "Jeroen"};
+	for (int i(0); i < len; i++) {
+		std::cout << names[i] << std::endl;
+		toupper(names[i]);
+		std::cout << names[i] << std::endl;
+	}
+	my_iter(names, len, &tolower);
+	for (int i(0); i < len; i++) {
+		std::cout << names[i] << std::endl;
+	}
 }
