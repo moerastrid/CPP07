1b87e77c47b9850809a0d08c9ceb5f611735cf78
moerastrid@gmail.com <moerastrid@gmail.com>
2023-06-24 14:44:20 +0200

    instantiated function template doesn't work

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ageels <ageels@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/06/07 16:48:07 by ageels        #+#    #+#                 */
-/*   Updated: 2023/06/24 14:24:13 by astrid        ########   odam.nl         */
+/*   Updated: 2023/06/24 14:40:10 by astrid        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,18 +30,40 @@ void tolower(std::string &s) {
 	}
 }
 
+void negative(int &num) {
+	num *= -1;
+}
+
+template<typename T>
+void display(T anything) {
+	std::cout << "Displaying : " << anything << std::endl;
+}
+
 int main(void) {
 	int len = 3;
-	
-	// int count[] = {1, 2, 3};
+	int count[] = {1, 2, 3};
 	std::string names[] = {"Astrid", "Riek", "Jeroen"};
 	for (int i(0); i < len; i++) {
 		std::cout << names[i] << std::endl;
-		toupper(names[i]);
+	}
+	my_iter(names, len, &toupper);
+	for (int i(0); i < len; i++) {
 		std::cout << names[i] << std::endl;
 	}
 	my_iter(names, len, &tolower);
 	for (int i(0); i < len; i++) {
 		std::cout << names[i] << std::endl;
 	}
+	for (int i(0); i < len; i++) {
+		std::cout << count[i] << std::endl;
+	}
+	my_iter(count, len, &negative);
+	for (int i(0); i < len; i++) {
+		std::cout << count[i] << std::endl;
+	}
+	const std::string cats[] = {"Foppe", "Loena"};
+	display(cats[1]);
+	toupper(names[1]);
+	display(count[2]);
+	my_iter(names, 2, &display);
 }
